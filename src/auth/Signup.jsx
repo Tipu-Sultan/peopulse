ec87b607b0d8cc61230e9ddf065e4938ea522cc6
Tipu Sultan <tipu@student.iul.ac.in>
2024-04-30 16:39:52 +0530

    fix again

--- src/auth/Signup.jsx
+++ src/auth/Signup.jsx
@@ -20,7 +20,7 @@ import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import { toast } from 'react-toastify';
-const API_HOST = process.env.REACT_APP_API_HOST || 'http://localhost:8080';
+const API_HOST = process.env.REACT_APP_API_HOST;
 
 export default function SignUp() {
   const [showPassword, setShowPassword] = useState(false);
